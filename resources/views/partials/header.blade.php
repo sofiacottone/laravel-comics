8d35694e8b84d32d4c564da036f2f11e9ac1108e
sofiacottone <cottonesofia@outlook.it>
2024-05-20 16:47:08 +0200

    feat: add header structure

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+    <nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary pb-0">
+        <div class="container-fluid">
+          <div class="container">
+            <div class="hstack justify-content-between">
+              <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                  aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <!-- navbar links -->
+                  <div class="navbar-nav text-uppercase ms-auto gap-3">
+                    @foreach ($navLinks as $navLink)
+                    <a class="nav-link d-flex align-items-center" href="#">{{ $navLink }}</a>
+                    @endforeach
+                  </div>
+              </div>
+            </div>
+        </div>
+        </div>
+      </nav>
+</header>
